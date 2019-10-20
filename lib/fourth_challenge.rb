a4987154c2b14647cd8df63c6aff9d2b524e4a07
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-10-20 17:18:29 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,8 +6,8 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-        :Benvolio => {name: "Benvolio", age: "17", attitude: "worried"}
-        :Mercutio => {name: "Mercutio", age: "18", attitude: "hot-headed"}
+        {name: "Benvolio", age: "17", attitude: "worried"}
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -16,7 +16,8 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        
+        {name: "Steven", age: "30", attitude: "confused"}
+        {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
